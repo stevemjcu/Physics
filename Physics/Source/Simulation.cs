cad045c23242b818a10a24cdfcdb30f5239aec90
Stephen Justice <stephenmjcu@gmail.com>
2026-01-21 18:29:05 -0500

    Add empty Step method

--- Physics/Source/Simulation.cs
+++ Physics/Source/Simulation.cs
@@ -2,5 +2,11 @@
 
 public class Simulation
 {
-
+    public void Step(TimeSpan timestep)
+    {
+        // Run integrator
+        // Detect collisions
+        // Run solver
+        // Update particles
+    }
 }
