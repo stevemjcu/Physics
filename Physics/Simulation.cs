eb51ef0b0fbe4e5b99f1e2682bc1d6de54a73914
Stephen Justice <stephenmjcu@gmail.com>
2026-02-13 18:34:09 -0500

    Add vector extensions class

--- Physics/Simulation.cs
+++ Physics/Simulation.cs
@@ -1,85 +1,93 @@
 ﻿using OpenTK.Mathematics;
 using Physics.Constraints;
 using Physics.Shapes;
+using System.Diagnostics;
 
 namespace Physics;
 
 public class Simulation
 {
-    public int Iterations { get; set; } = 20;
+	public int Iterations { get; set; } = 20;
 
-    public float DampingCoefficient { get; set; } = 1;
+	public float DampingCoefficient { get; set; } = 1;
 
-    public float FrictionCoefficient { get; set; } = 1;
+	public float FrictionCoefficient { get; set; } = 1;
 
-    public float Gravity { get; set; } = 10;
+	public float Gravity { get; set; } = 10;
 
-    public List<Particle> Particles { get; set; } = [];
+	public List<Particle> Particles { get; set; } = [];
 
-    public List<Constraint> Constraints { get; set; } = [];
+	public List<Constraint> Constraints { get; set; } = [];
 
-    public List<Collider> Colliders { get; set; } = [];
+	public List<Collider> Colliders { get; set; } = [];
 
-    public List<CollisionConstraint> CollisionConstraints { get; set; } = [];
+	public List<CollisionConstraint> CollisionConstraints { get; set; } = [];
 
-    public void Step(float timestep)
-    {
-        // Integrate particles
-        foreach (var it in Particles)
-        {
-            if (it.HasGravity)
-            {
-                it.Velocity += new Vector3(0, -Gravity, 0) * timestep;
-            }
+	public void Step(float timestep)
+	{
+		// Integrate particles
+		foreach (var it in Particles)
+		{
+			if (it.HasGravity)
+			{
+				it.Velocity += Vector3.Zero with { Y = -Gravity } * timestep;
+			}
 
-            it.PreviousPosition = it.Position;
-            it.Position += it.Velocity * timestep;
-        }
+			it.PreviousPosition = it.Position;
+			it.Position += it.Velocity * timestep;
+		}
 
-        // Generate collision constraints
-        foreach (var it in Particles)
-        {
-            foreach (var jt in Colliders)
-            {
-                var ray = new Ray(it.PreviousPosition, it.Position);
-                var length = (it.Position - it.PreviousPosition).Length;
+		// Generate collision constraints
+		foreach (var it in Particles)
+		{
+			foreach (var jt in Colliders)
+			{
+				var ray = new Ray(it.PreviousPosition, it.Position);
+				var length = (it.Position - it.PreviousPosition).Length;
 
-                if (ray.Overlaps(jt.Triangle, out var t) && t <= length)
-                {
-                    CollisionConstraints.Add(new(it, ray.GetPoint(t), jt.Triangle.Normal, 1));
-                }
-            }
-        }
+				if (ray.Overlaps(jt.Triangle, out var t) && t <= length)
+				{
+					// TODO: Use pool to avoid allocation
+					CollisionConstraints.Add(new(it, ray.GetPoint(t), jt.Triangle.Normal, 1));
+				}
+			}
+		}
 
-        // Solve constraints
-        for (var i = 0; i < Iterations; i++)
-        {
-            foreach (var it in Constraints)
-            {
-                it.Project();
-            }
+		// Solve constraints
+		for (var i = 0; i < Iterations; i++)
+		{
+			foreach (var it in Constraints)
+			{
+				it.Project();
+			}
 
-            foreach (var it in CollisionConstraints)
-            {
-                it.Project();
-            }
-        }
+			foreach (var it in CollisionConstraints)
+			{
+				it.Project();
+			}
+		}
 
-        // Derive velocities
-        foreach (var it in Particles)
-        {
-            it.Velocity = (it.Position - it.PreviousPosition) / timestep;
-            it.Velocity *= DampingCoefficient;
-        }
+		// Derive velocities
+		foreach (var it in Particles)
+		{
+			it.Velocity = (it.Position - it.PreviousPosition) / timestep;
+			it.Velocity *= DampingCoefficient;
+		}
 
-        foreach (var it in CollisionConstraints)
-        {
-            var a = Vector3.Dot(it.Particles[0].Velocity, it.Normal) * it.Normal; // along normal
-            var b = (it.Particles[0].Velocity - a) * FrictionCoefficient; // tangential to normal
+		foreach (var it in CollisionConstraints)
+		{
+			var velocity = it.Particles[0].Velocity;
+			var a = velocity.Along(it.Normal, out var b);
 
-            it.Particles[0].Velocity = a + b;
-        }
+#if DEBUG
+			Debug.Assert(a + b == velocity);
+			Debug.Assert(Vector3.Dot(a, b) == 0);
+			Debug.Assert(Vector3.Dot(b, it.Normal) == 0);
+#endif
 
-        CollisionConstraints.Clear();
-    }
+			it.Particles[0].Velocity = a + b * FrictionCoefficient;
+		}
+
+		CollisionConstraints.Clear();
+	}
 }
---
+++ Physics/VectorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK.Mathematics;
+
+namespace Physics;
+
+public static class VectorExtensions
+{
+	public static Vector3 Along(this Vector3 a, Vector3 b, out Vector3 orthogonal)
+	{
+		var unit = b.Normalized();
+		var along = Vector3.Dot(a, unit) * unit;
+		orthogonal = (a - along);
+		return along;
+	}
+}
