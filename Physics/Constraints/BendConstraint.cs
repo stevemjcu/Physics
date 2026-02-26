f4161db67f40dddf571045c07132c6da8890395f
Stephen Justice <stephenmjcu@gmail.com>
2026-02-25 20:45:46 -0500

    Add empty bend constraint

---
+++ Physics/Constraints/BendConstraint.cs
@@ -0,0 +1,14 @@
+﻿using Physics.Colliders;
+
+namespace Physics.Constraints;
+
+public class BendConstraint(TriangleCollider source, TriangleCollider target, float distance, float compliance)
+    : Constraint([.. source.Particles, target.Particles[^1]], compliance)
+{
+    public float Distance { get; set; } = distance;
+
+    public override void CalculateError()
+    {
+
+    }
+}
