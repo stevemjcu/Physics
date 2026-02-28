86a28f7efbee80900e62e09bc3df585378679519
Stephen Justice <stephenmjcu@gmail.com>
2026-02-27 22:24:30 -0500

    Try implementing dihedral constraint

---
+++ Physics/Constraints/DihedralConstraint.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Mathematics;
+using Physics.Shapes;
+
+namespace Physics.Constraints;
+
+/// <summary>
+/// A constraint on the angle between two adjacent triangles ACB and ABD with the shared edge AB.
+/// </summary>
+public class DihedralConstraint(Particle a, Particle b, Particle c, Particle d, float angle, float compliance)
+    : Constraint([a, b, c, d], compliance)
+{
+    public float Angle { get; set; } = angle;
+
+    public override void CalculateError()
+    {
+        var source = new Triangle(a.Position, c.Position, b.Position);
+        var target = new Triangle(a.Position, b.Position, d.Position);
+        var angle = Vector3.CalculateAngle(source.Normal, target.Normal);
+
+        // C and D should move along CD (opposite directions)
+        // A and B should move perpendicular to CD and AB (same direction)
+
+        var ab = b.Position - a.Position;
+        var cd = d.Position - c.Position;
+
+        Error = angle - Angle;
+        Gradient[0] = Vector3.Cross(ab, cd);
+        Gradient[1] = Gradient[0];
+        Gradient[2] = cd;
+        Gradient[3] = -Gradient[2];
+    }
+}
